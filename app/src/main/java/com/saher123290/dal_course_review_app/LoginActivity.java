a8e866351da08515301e58fc9ff10dd79ac94af7
SaherPersonal <saher.ziauddin@dal.ca>
2022-04-07 18:33:11 -0300

    - created LoginActivity and transferred interface implementations to MainActivity

---
+++ app/src/main/java/com/saher123290/dal_course_review_app/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.saher123290.dal_course_review_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.saher123290.dal_course_review_app.View.ILoginView;
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
