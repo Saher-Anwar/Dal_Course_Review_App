3b9192a8313c0e08456267b05a614920be75de4e
SaherPersonal <saher.ziauddin@dal.ca>
2022-04-07 18:34:10 -0300

    - Login activity implements ILoginView

--- app/src/main/java/com/saher123290/dal_course_review_app/MainActivity.java
+++ app/src/main/java/com/saher123290/dal_course_review_app/MainActivity.java
@@ -7,21 +7,11 @@ import android.widget.Toast;
 
 import com.saher123290.dal_course_review_app.View.ILoginView;
 
-public class MainActivity extends AppCompatActivity implements ILoginView {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public void OnLoginSuccess(String message) {
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void OnLoginError(String message) {
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
