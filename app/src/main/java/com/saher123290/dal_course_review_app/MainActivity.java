77cb714d9e87911f6198da8ef37bd47e2825b588
SaherPersonal <saher.ziauddin@dal.ca>
2022-04-07 02:44:03 -0300

    - initial commit

---
+++ app/src/main/java/com/saher123290/dal_course_review_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saher123290.dal_course_review_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
